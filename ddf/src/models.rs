9758ce6a056b3c4b3c16595c2d82716cd4a0534f
jakob753951 <jlm1999@live.dk>
2024-05-16 08:01:27 +0200

    - Added postgres feature to sqlx - Added models to ddf

---
+++ ddf/src/models.rs
@@ -0,0 +1,8 @@
+pub struct DDF {
+    pub id: String,
+    pub device_type: String,
+    pub sku_number: String,
+    pub manufacturer: String,
+    pub model: String,
+    pub dce_serial: String,
+}
