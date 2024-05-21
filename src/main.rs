f2f4702d29c71442e18656240a7d39c6e2117103
jakob753951 <jlm1999@live.dk>
2024-05-21 23:15:16 +0200

    - Fixed cargo.tomls - added URLs to ddf's config.toml

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use pim;
-use pim::models::Product;
+use pim::Product;
 use gps;
 use ddf;
-use ddf::models::NewDDF;
+use ddf::NewDDF;
 use color_eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Hello, world!");
 
-    let pim_conn: pim::get_pool().await?;
-    let products: Vec<Product> = pim::get_all_products(pim_conn).await?;
+    // let pim_conn: pim::get_pool().await?;
+    // let products: Vec<Product> = pim::get_all_products(pim_conn).await?;
+    //
+    // let new_ddfs = products.map(NewDDF::from).collect();
+    //
+    // let ddf_conn = ddf::get_pool().await?;
+    // let matching_ddfs = ddf::get_matching_ddfs(&ddf_conn, new_ddfs).await?;
 
-    let new_ddfs = products.map(NewDDF::from).collect();
-
-    let ddf_conn = ddf::get_pool().await?;
-    let matching_ddfs = ddf::get_matching_ddfs(&ddf_conn, new_ddfs).await?;
+    Ok(())
 }
