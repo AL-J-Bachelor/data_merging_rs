3451306d69cfafd2d3f8d219c19b382aa6c6fec8
jakob753951 <jlm1999@live.dk>
2024-05-27 09:25:21 +0200

    - add more info to Product struct

--- models/src/pim.rs
+++ models/src/pim.rs
@@ -1,8 +1,14 @@
 use poem_openapi::Object;
 use serde::{Deserialize, Serialize};
 
+pub type Millimeters = f32;
+
 #[derive(Object)]
 #[derive(Serialize, Deserialize)]
 pub struct Product {
     pub id: i32,
+    pub manufacturer: String,
+    pub width: Millimeters,
+    pub height: Millimeters,
+    pub depth: Millimeters,
 }
