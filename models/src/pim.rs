301de3d074a7af01246ef701b05fd8075e6b280b
Jakob Ladegaard Møller <jakob.moeller@non.se.com>
2024-05-30 11:04:43 +0200

    - rename device_type to type - add NewProduct - split Dimensions into new struct - add create_product endpoint

--- models/src/pim.rs
+++ models/src/pim.rs
@@ -1,18 +1,46 @@
 use poem_openapi::Object;
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+
+#[derive(Object)]
+#[derive(Serialize, Deserialize)]
+#[derive(FromRow)]
+#[derive(Clone)]
+pub struct Product {
+    pub id: i32,
+    pub sku_number: String,
+    #[sqlx(rename = "type")]
+    pub device_type: String,
+    pub manufacturer: String,
+    pub model: Option<String>,
+    pub dce_serial_number: String,
+    #[sqlx(flatten)]
+    pub dimensions: Dimensions,
+}
+
+#[derive(Object)]
+#[derive(Serialize, Deserialize)]
+#[derive(FromRow)]
+#[derive(Clone)]
+pub struct NewProduct {
+    pub sku_number: String,
+    #[sqlx(rename = "type")]
+    pub device_type: String,
+    pub manufacturer: String,
+    pub model: Option<String>,
+    pub dce_serial_number: String,
+    #[sqlx(flatten)]
+    pub dimensions: Dimensions,
+}
 
 pub type Millimeters = f64;
 
 #[derive(Object)]
 #[derive(Serialize, Deserialize)]
+#[derive(FromRow)]
 #[derive(Clone)]
-pub struct Product {
-    pub id: i32,
-    pub sku_number: String,
-    pub device_type: String,
-    pub manufacturer: String,
-    pub model: Option<String>,
-    pub dce_serial_number: String,
+pub struct Dimensions {
     pub width: Millimeters,
     pub height: Millimeters,
     pub depth: Millimeters,
--- pim/src/endpoints.rs
+++ pim/src/endpoints.rs
@@ -4,7 +4,7 @@ use poem::Result;
 use poem_openapi::OpenApi;
 use poem_openapi::payload::Json;
 use sqlx::PgPool;
-use models::pim::Product;
+use models::pim::{NewProduct, Product};
 
 pub struct Api;
 
@@ -13,12 +13,36 @@ impl Api {
     /// Get all Products
     #[oai(path = "/products", method = "get")]
     pub async fn get_products(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Product>>> {
-        let products = sqlx::query_as!(
-            Product,
+        let products = sqlx::query_as(
             r#"
-                SELECT id, sku_number, device_type, manufacturer, model, dce_serial_number, width, height, depth FROM products
+                SELECT id, sku_number, type, manufacturer, model, dce_serial_number, width, height, depth FROM products
             "#
         )
+            .fetch_all(pool.0)
+            .await
+            .map_err(|e| InternalServerError(e))?;
+
+        Ok(Json(products))
+    }
+
+    /// Insert product
+    #[oai(path = "/products", method = "put")]
+    pub async fn create_product(&self, pool: Data<&PgPool>, product: Json<NewProduct>) -> Result<Json<Vec<Product>>> {
+        let products = sqlx::query_as(
+            r#"
+                INSERT INTO products (sku_number, type, manufacturer, model, dce_serial_number, width, height, depth)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                ON CONFLICT ON CONSTRAINT products_sku_number_manufacturer_model_key DO UPDATE SET model = EXCLUDED.model
+                RETURNING id, sku_number, type, manufacturer, model, dce_serial_number, width, height, depth
+            "#)
+            .bind(&product.sku_number)
+            .bind(&product.device_type)
+            .bind(&product.manufacturer)
+            .bind(&product.model)
+            .bind(&product.dce_serial_number)
+            .bind(&product.dimensions.width)
+            .bind(&product.dimensions.height)
+            .bind(&product.dimensions.depth)
             .fetch_all(pool.0)
             .await
             .map_err(|e| InternalServerError(e))?;
@@ -29,11 +53,10 @@ impl Api {
     /// Delete all Products
     #[oai(path = "/products", method = "delete")]
     pub async fn delete_all_products(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Product>>> {
-        let deleted_products = sqlx::query_as!(
-            Product,
+        let deleted_products = sqlx::query_as(
             r#"
                 DELETE FROM products
-                RETURNING id, sku_number, device_type, manufacturer, model, dce_serial_number, width, height, depth
+                RETURNING id, sku_number, type, manufacturer, model, dce_serial_number, width, height, depth
             "#
         )
             .fetch_all(pool.0)
